b21bf6e3a35155b57bc47541b17729ed2a551a43
nsgw <nickgoespop@gmail.com>
2022-01-20 15:23:43 +0000

    Made it fully functional & fixed errors.
    Cleaned up the code too.

--- NaughtsAndCrosses.cs
+++ NaughtsAndCrosses.cs
@@ -2,34 +2,30 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 // Also known as Tic Tac Toe
 namespace NaughtsAndCrosses
 {
     class Program
     {
-        static void Main(string[] args)
+        private static int[] _board;
+        private static int _size;
+        private static void Main(string[] args)
         {
             bool playAgain;
-            int[] board;
-            int size;
-            int current;
-            string choice;
-            int won, stop;
             do
             {
-                setSize(out size);
-                current = 1;
-                won = -1;
-                board = empty(size);
-                stop = 0;
+                SetSize();
+                int won = -1, stop = 0, current = 1;
+                _board = Empty();
                 while(won < 0)
                 {
-                    writeBoard(board, size);
-                    Console.WriteLine("Turn: " + getChar(current));
+                    WriteBoard();
+                    Console.WriteLine("Turn: " + GetChar(current));
                     Console.WriteLine("Column, row (layout: n,n. E.g. 1,2)");
-                    choice = Console.ReadLine();
-                    while (!processChoice(current, choice, out board, board, size, out stop))
+                    var choice = Console.ReadLine();
+                    while (!ProcessChoice(current, choice, out stop))
                     {
                         Console.WriteLine("Invalid value.");
                         choice = Console.ReadLine();
@@ -37,59 +33,44 @@ namespace NaughtsAndCrosses
                     if (stop > 0)
                         break;
                     current = current == 1 ? 2 : 1;
-                    checkForWin(board, size, out won);
+                    CheckForWin(out won);
                 }
 
                 if (stop == 2)
                     break;
-                else if (stop == 1)
+                if (stop == 1)
                 {
                     playAgain = true;
                 }
                 else
                 {
-                    writeBoard(board, size);
-                    Console.WriteLine(won == 0 ? "Draw!" : "The winner is player #" + won + "!");
+                    WriteBoard();
+                    Console.WriteLine(won == 0 ? "Draw!" : "The winner is player #" + won + " (" + GetPlayer(won) + ")!");
                     Console.WriteLine("Do you want to play again? (y/n...)");
                     playAgain = Console.ReadKey().KeyChar == 'y';
                 }
             } while (playAgain);
         }
 
-        static void setSize(out int size)
+        private static void SetSize()
         {
             Console.WriteLine("Choose board size (default 3, min. 3, max 9):");
-            if (!Int32.TryParse(Console.ReadLine(), out size))
-            {
-                size = 3;
-            }
-            else if (size > 9)
-            {
-                size = 9;
-            }
-            else if (size < 3)
-            {
-                size = 3;
-            }
+            _size = !int.TryParse(Console.ReadLine(), out _size) ? 3 : _size > 9 ? 9 : _size < 3 ? 3 : _size;
         }
-        static bool processChoice(int player, string choice, out int[] boardOut, int[] board, int size, out int stop)
+        private static bool ProcessChoice(int player, string choice, out int stop)
         {
-            int[] newBoard = board;
             if (choice.ToLower().StartsWith("s"))
             {
-                boardOut = newBoard;
                 stop = 0;
                 return true;
             }
             if (choice.ToLower().StartsWith("r"))
             {
-                boardOut = newBoard;
                 stop = 1;
                 return true;
             }
             if (choice.ToLower().StartsWith("q"))
             {
-                boardOut = newBoard;
                 stop = 2;
                 return true;
             }
@@ -97,62 +78,54 @@ namespace NaughtsAndCrosses
             string[] split = choice.Split(',');
             if(split.Length < 2)
             {
-                boardOut = newBoard;
                 return false;
             }
-            int row,
-                col;
-            if(!Int32.TryParse(split[1].Trim(), out row)) {
-                boardOut = newBoard;
+            if(!int.TryParse(split[1].Trim(), out int row)) {
                 return false;
             } 
-            else if (!Int32.TryParse(split[0].Trim(), out col))
+            if (!int.TryParse(split[0].Trim(), out int col))
             {
-                boardOut = newBoard;
                 return false;
             }
-            if(!((0 < row && row <= size) && (0 < col && col <= size)))
+            if(!(0 < row && row <= _size && 0 < col && col <= _size))
             {
-                boardOut = board;
                 return false;
             }
             row--;
             col--;
-            int bidding = board[row * size + col];
-            if(!checkForPlayer(bidding))
+            var bidding = _board[row * _size + col];
+            if(!CheckForPlayer(bidding))
             {
-                newBoard.SetValue(player,(row * size) + col);
-                boardOut = newBoard;
+                _board.SetValue(player,(row * _size) + col);
                 return true;
             } 
             Console.WriteLine("That box is taken.");
-            boardOut = board;
             return false;
         }
 
         // size must be a factor of board.Length
-        static void writeBoard(int[] board, int size)
+        private static void WriteBoard()
         {
             Console.Clear();
             Console.WriteLine("Type s to skip, r to restart or q to quit.\n");
-            string line, spacer = new string('-', (size * 4) + 2);
-            for(int i = 1; i <= size; i++)
+            var spacer = new string('-', _size * 4 + 2);
+            for(var i = 1; i <= _size; i++)
             {
                 Console.Write("---" + i);
             }
             Console.Write("--\n");
-            for(int i = 0; i < size; i++)
+            for(int i = 0; i < _size; i++)
             {
-                line = i + 1 + "|";
-                for(int j = 0; j < size; j++)
+                var line = i + 1 + "|";
+                for(int j = 0; j < _size; j++)
                 {
-                    line += " " + getChar(board[(i * size) + j]) + " |";
+                    line += " " + GetChar(_board[i * _size + j]) + " |";
                 }
                 Console.WriteLine(line);
                 Console.WriteLine(spacer);
             }
         }
-        static char getChar(int n)
+        private static char GetChar(int n)
         {
             switch(n)
             {
@@ -163,82 +136,78 @@ namespace NaughtsAndCrosses
             }
             return ' ';
         }
-        static char getPlayer(int current)
+        private static char GetPlayer(int current)
         {
-            return getChar(current);
+            return GetChar(current);
         }
-        static bool checkForWin(int[] board, int size, out int winner)
+        private static void CheckForWin(out int winner)
         {
-            List<int> verticalSquares, horizontalSquares;
-            for(int i = 0; i < size; i++)
+            for(var i = 0; i < _size; i++)
             {
-                verticalSquares = new List<int>();
-                horizontalSquares = new List<int>();
-                for (int j = 0; j < size; j++)
+                List<int> verticalSquares = new List<int>(),
+                    horizontalSquares = new List<int>();
+                for (var col = 0; col < _size; col++)
                 {
-                    verticalSquares.Add(board[i + size * j]);
-                    horizontalSquares.Add(board[i + j]);
+                    verticalSquares.Add(_board[i + col * _size]);
+                    horizontalSquares.Add(_board[(i * _size) + col]);
                 }
-                if (!checkForPlayer(board[i])) continue;
-                if ((size % (i + 1) == 0
-                    && squaresMatch(horizontalSquares.ToArray()))
-                    || squaresMatch(verticalSquares.ToArray()))
+                if (SquaresMatch(horizontalSquares.ToArray()) && CheckForPlayer(horizontalSquares[0]))
                 {
-                    winner = getChar(board[i]) == 'O' ? 1 : 2;
-                    return true;
+                    winner = horizontalSquares[0];
+                }
+                else if (SquaresMatch(verticalSquares.ToArray()) && CheckForPlayer(verticalSquares[0]))
+                {
+                    winner = verticalSquares[0];
+                    return;
                 }
             }
-            List<int> squaresFromDiagonal = new List<int>();
-            for(int i = 0; i < size; i++)
+            var squaresFromDiagonal = new List<int>();
+            for(var i = 0; i < _size; i++)
             {
-                squaresFromDiagonal.Add(board[i]);
+                squaresFromDiagonal.Add(_board[i * (_size + 1)]);
             }
-            if (squaresMatch(squaresFromDiagonal.ToArray()) && checkForPlayer(squaresFromDiagonal[0]))
+            if (SquaresMatch(squaresFromDiagonal.ToArray()) && squaresFromDiagonal.TrueForAll(CheckForPlayer))
             {
                 winner = squaresFromDiagonal[0];
-                return true;
+                return;
             }
             squaresFromDiagonal = new List<int>();
-            for (int i = size - 1; i >= 0; i--)
+            for (int i = _size - 1; i <= _size * (_size - 1); i += (_size - 1))
             {
-                squaresFromDiagonal.Add(board[i]);
+                squaresFromDiagonal.Add(_board[i]);
             }
-            if (squaresMatch(squaresFromDiagonal.ToArray()) && checkForPlayer(squaresFromDiagonal[size - 1]))
+            if (SquaresMatch(squaresFromDiagonal.ToArray()) && squaresFromDiagonal.TrueForAll(CheckForPlayer))
             {
-                winner = squaresFromDiagonal[size - 1];
-                return true;
+                winner = squaresFromDiagonal[0];
+                return;
             }
-            foreach(int i in board)
+            if (!Array.TrueForAll(_board, CheckForPlayer))
             {
-                if(!checkForPlayer(i))
-                {
-                    winner = -1;
-                    return false;
-                }
+                winner = -1;
+                return;
             }
             winner = 0;
-            return true;
         }
-        static bool checkForPlayer(int n)
+        private static bool CheckForPlayer(int n)
         {
             return n == 1 || n == 2;
         }
-        static bool squaresMatch(params int[] squares)
+        private static bool SquaresMatch(params int[] squares)
         {
-            int previous = squares[0];
-            for (int i = 0; i < squares.Length; i++)
+            var previous = squares[0];
+            foreach (var square in squares)
             {
-                if (previous != squares[i]) return false;
-                previous = squares[i];
+                if (previous != square) return false;
+                previous = square;
             }
             return true;
         }
-        static int[] empty(int size)
+        private static int[] Empty()
         {
-            List<int> list = new List<int>();
-            for(int i = 0; i < size * size; i++)
+            var list = new List<int>();
+            for(var i = 0; i < _size * _size; i++)
             {
-                list.Add(getChar(0));
+                list.Add(GetChar(0));
             }
             return list.ToArray();
         }
